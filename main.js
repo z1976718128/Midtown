abd3f1c59b3a7b0206689599934cc30ccd271969
z1976718128 <1976718128@qq.com>
2020-01-06 15:23:57 +0800

    zjy1.6

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 import Vue from 'vue'
 import App from './App'
 import router from '@/router/index.js'
+import MtaH5 from "mta-h5-analysis";
 Vue.config.productionTip = false
 App.mpType = 'app'
 
 let wx = require('jweixin-module')
 let url =window.location.href;
+
+// 初始化
+MtaH5.init({
+sid: "500708274", //必填，统计用的appid
+// cid: "********", //如果开启自定义事件，此项目为必填，否则不填
+autoReport: 0, //是否开启自动上报(1:init完成则上报一次,0:使用pgv方法才上报)
+senseHash: 1, //hash锚点是否进入url统计
+senseQuery: 1, //url参数是否进入url统计
+performanceMonitor: 1, //是否开启性能监控
+ignoreParams: ["token", "v", "from"] //开启url参数上报时，可忽略部分参数拼接上报
+});
+
 uni.request({
 	url:"http://zc.demo.yudw.com/api/index/getSignPackage",
 	method: 'get',
